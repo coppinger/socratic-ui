265e1c718f1a931b9ed57cebf04b1a62d63db250
coppinger <charles.coppinger@gmail.com>
2026-04-13 13:16:27 +1200

    fix bg image shader scroll issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased overscroll-none`}
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider
